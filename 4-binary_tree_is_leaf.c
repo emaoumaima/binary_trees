a09e3620dcde02f2fd26d2018dd1205475ce5d22
emaoumaima <sellouane.oumaima@gmail.com>
2024-01-03 04:52:10 -0800

    fix: betty style

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,5 +10,5 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (!node)
 		return 0;
-	return !(node->left || node->right);
+	return (!(node->left || node->right));
 }
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -10,5 +10,5 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (!node)
 		return 0;
-	return !node->parent;
+	return (!node->parent);
 }
