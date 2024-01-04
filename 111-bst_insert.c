aaa335f270302acef37b8859a812c5e053f09c3d
emaoumaima <sellouane.oumaima@gmail.com>
2024-01-04 12:54:44 -0800

    fix betty

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,57 +1,52 @@
 #include "binary_trees.h"
 
-typedef struct binary_tree_s bst_t;
-
 /**
  * bst_insert - Inserts a value in a Binary Search Tree.
- * @tree: Double pointer to the root node of the BST to insert the value.
+ * @tree: Double pointer to the root node of the BST.
  * @value: Value to store in the node to be inserted.
  * Return: A pointer to the created node, or NULL on failure.
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-    if (tree == NULL)
-        return (NULL);
+	if (tree == NULL)
+		return (NULL);
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
+	bst_t *current = *tree;
 
-    if (*tree == NULL)
-    {
-        *tree = binary_tree_node(NULL, value);
-        return (*tree);
-    }
-
-    bst_t *current = *tree;
-
-    while (current != NULL)
-    {
-        if (value < current->n)
-        {
-            if (current->left == NULL)
-            {
-                current->left = binary_tree_node(current, value);
-                return (current->left);
-            }
-            else
-            {
-                current = current->left;
-            }
-        }
-        else if (value > current->n)
-        {
-            if (current->right == NULL)
-            {
-                current->right = binary_tree_node(current, value);
-                return (current->right);
-            }
-            else
-            {
-                current = current->right;
-            }
-        }
-        else
-        {
-            return (NULL);
-        }
-    }
-
-    return (NULL);
+	while (current != NULL)
+	{
+		if (value < current->n)
+		{
+			if (current->left == NULL)
+			{
+				current->left = binary_tree_node(current, value);
+				return (current->left);
+			}
+			else
+			{
+				current = current->left;
+			}
+		}
+		else if (value > current->n)
+		{
+			if (current->right == NULL)
+			{
+				current->right = binary_tree_node(current, value);
+				return (current->right);
+			}
+			else
+			{
+				current = current->right;
+			}
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+	return (NULL);
 }
